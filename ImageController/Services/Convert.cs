843637c27cbdaac0a8ee032f2d410cbf26a093cd
Floran <floran.houdart@gmail.com>
2022-12-08 10:55:27 +0100

    convert

---
+++ ImageController/Services/Convert.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using Image = System.Drawing.Image;
+
+namespace ImageController.Services
+{
+    public class Convert
+    {
+        public static Image ImgToByte(byte[] image, ImageFormat pFormat)
+        {
+            var imageObject = new Bitmap(new MemoryStream(image));
+
+            var stream = new MemoryStream();
+            imageObject.Save(stream, pFormat);
+
+            return new Bitmap(stream);
+        }
+    }
+}
